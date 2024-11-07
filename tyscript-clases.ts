b227b0283a826684575a14c99fa8ec530af5fe64
Sol Soletti <soolaltair@gmail.com>
2024-11-06 21:52:43 -0300

    Inicialización entregable 2

---
+++ tyscript-clases.ts
@@ -0,0 +1,13 @@
+/*1. Pila básica (LIFO)
+Una pila tiene tres operaciones básicas:
+
+push: Agregar un elemento a la pila.
+pop: Sacar el último elemento agregado de la pila.
+size: Consultar la cantidad de elementos en la pila.*/
+
+/*Los genéricos ofrecen una forma de crear componentes reutilizables
+▶ Además proporcionan una manera de hacer que los componentes funcionen con
+cualquier tipo de datos y no se restrinjan a un tipo de datos particular
+▶ Por lo tanto, los componentes se pueden llamar o usar con una variedad de tipos
+de datos*/
+
